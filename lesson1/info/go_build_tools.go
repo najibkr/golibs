98ccd910d786d1109c6938d1dfc5ffd402138244
Najibullah Khoda Rahim <parwandaha@gmail.com>
2020-07-10 07:04:21 +0800

    info pacakge added

---
+++ lesson1/info/go_build_tools.go
@@ -0,0 +1,7 @@
+package info
+
+// RunGoBuildToolsInfo prints out information about Go Build Tools in the terminal
+func RunGoBuildToolsInfo() {
+	print("run <go version> command to see the current installed version of go")
+	print("")
+}
