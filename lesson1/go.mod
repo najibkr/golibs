9b291dbefad956def6f050075d07231b0db0c4c4
Najibullah Khoda Rahim <parwandaha@gmail.com>
2020-07-10 08:41:34 +0800

    Hello

--- lesson1/go.mod
+++ lesson1/go.mod
@@ -2,4 +2,7 @@ module github.com/najibkr/golibs/lesson1
 
 go 1.14
 
-require golang.org/x/tools/gopls v0.4.3 // indirect
+require (
+	golang.org/x/tools v0.0.0-20200709181711-e327e1019dfe // indirect
+	golang.org/x/tools/gopls v0.4.3 // indirect
+)
--- lesson1/info/go_build_tools.go
+++ lesson1/info/go_build_tools.go
@@ -5,5 +5,7 @@ import "fmt"
 // RunGoBuildToolsInfo prints out information about Go Build Tools in the terminal
 func RunGoBuildToolsInfo() {
 	fmt.Println("run <go version> command to see the current installed version of go")
-	fmt.Println("")
+	fmt.Println("run go to see all the related commands to Go")
+	fmt.Println("run go env to see the environment variables of Go")
+	fmt.Println("Thre are executable Pacakges and library packages")
 }
